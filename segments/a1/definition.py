1a6337030cbf74afb9911ba7f7f123371d828712
josiah-wolf-oberholtzer <josiah.oberholtzer@gmail.com>
2013-12-29 17:17:22 -0500

    Updated class layout of first concrete segment.

--- segments/a1/definition.py
+++ segments/a1/definition.py
@@ -6,35 +6,29 @@ from plague_water import materials
 
 class Segment(callables.SegmentMaker):
 
-    measure_segmentation_talea = (1,)
-
-    minimum_timespan_duration = Duration(3, 16)
-
-    permitted_time_signatures = datastructuretools.TypedList(
-        [
-            (5, 16),
-            (3, 8),
-            (7, 16),
-            (2, 4),
-            (4, 8),
-            (5, 8),
-            (3, 4),
-            (6, 8),
-            ],
-        item_class=TimeSignature,
-        )
-
-    playing_duration_cursor = materials.medium_duration_server()
-
-    playing_grouping_cursor = materials.medium_grouping_server()
-
-    resting_duration_cursor = materials.medium_duration_server(reverse=True)
-
     segment_name = 'Segment One'
 
-    segment_target_duration = Duration(8)
+    segment_target_duration = Duration(6)
 
     tempo = materials.tempo_inventory[0]
+
+    ### VOICE-LEVEL VARIABLES ###
+
+    guitar_brush = None
+
+    guitar_lifeline_strategy = None
+
+    piano_lh_brush = None
+
+    piano_lifeline_strategy = None
+
+    piano_rh_brush = None
+
+    percussion_lh_brush = None
+
+    percussion_rh_brush = None
+
+    saxophone_brush = None
 
 
 if __name__ == '__main__':
