72175991b5a0841e8e1cc51915987aed420c3b78
josiah-wolf-oberholtzer <josiah.oberholtzer@gmail.com>
2013-12-30 12:21:13 -0500

    Added euler numbers material.

---
+++ materials/euler_numbers/__metadata__.py
@@ -0,0 +1,9 @@
+# -*- encoding: utf-8 -*-
+import collections
+
+
+tags = collections.OrderedDict([
+    ('is_material_package', True),
+    ('material_package_maker_class_name', None),
+    ('should_have_illustration', False),
+    ('should_have_user_input_module', False)])
