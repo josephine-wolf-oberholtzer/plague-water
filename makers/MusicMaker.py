01126641cccf8089a08e9a06c875434c03790b45
josiah-wolf-oberholtzer <josiah.oberholtzer@gmail.com>
2013-12-31 15:55:55 -0500

    Fleshed out MusicMaker interface.

--- makers/MusicMaker.py
+++ makers/MusicMaker.py
@@ -3,4 +3,24 @@ from abjad import *
 
 
 class MusicMaker(abctools.AbjadObject):
-    pass
+
+    ### CLASS VARIABLES ###
+
+    __slots__ = (
+        )
+
+    ### INITIALIZER ###
+
+    def __init__(self):
+        pass
+
+    ### SPECIAL METHODS ###
+
+    def __call__(self, divisions, context_name=None, context_tree=None):
+        pass
+
+    def __eq__(self, expr):
+        pass
+
+    def __hash__(self, expr):
+        pass
