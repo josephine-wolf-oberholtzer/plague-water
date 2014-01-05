de1a11d7086020ace9d18d26276a67a3ae6e77bf
Josiah Wolf Oberholtzer <josiah.oberholtzer@gmail.com>
2014-01-04 21:17:09 -0500

    Added stub material packages.

---
+++ materials/saxophone_brushes/material_definition.py
@@ -0,0 +1,6 @@
+# -*- encoding: utf-8 -*-
+from abjad import *
+output_material_module_import_statements = ['from abjad import *']
+
+
+/home/josiah/Documents/Scores/plague_water/materials/saxophone_brushes = None
