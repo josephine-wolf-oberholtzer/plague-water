0e3a5ab23eefde9ecd79edca790c4164b45afa36
Josiah Wolf Oberholtzer <josiah.oberholtzer@gmail.com>
2014-01-04 22:07:05 -0500

    Fixed syntax errors in materials packages.

--- materials/percussion_brushes/material_definition.py
+++ materials/percussion_brushes/material_definition.py
@@ -1,6 +1,2 @@
 # -*- encoding: utf-8 -*-
 from abjad import *
-output_material_module_import_statements = ['from abjad import *']
-
-
-/home/josiah/Documents/Scores/plague_water/materials/percussion_brushes = None
