8ce06b4be304124b34ddeb63dbacd082243138bd
josiah-wolf-oberholtzer <josiah.oberholtzer@gmail.com>
2014-01-06 18:57:24 -0500

    Wrote time signature inventories.

--- materials/time_signature_inventories/material_definition.py
+++ materials/time_signature_inventories/material_definition.py
@@ -1,2 +1,43 @@
 # -*- encoding: utf-8 -*-
 from abjad import *
+
+
+time_signature_inventories = datastructuretools.TypedTuple(
+    tokens=[
+        [
+            (5, 16),
+            (7, 16),
+            (2, 4),
+            (4, 8),
+            (5, 8),
+            (3, 4),
+            (6, 8),
+            ],
+        [
+            (3, 4),
+            (3, 8),
+            (6, 8),
+            (9, 16),
+            ],
+        [
+            (2, 4),
+            (4, 16),
+            (4, 8),
+            ],
+        [
+            (5, 16),
+            (5, 8),
+            (7, 16),
+            ],
+        [
+            (1, 8),
+            (3, 4),
+            (4, 4),
+            ],
+        ],
+    item_class=indicatortools.TimeSignatureInventory,
+    )
+
+__all__ = [
+    'time_signature_inventories',
+    ]
