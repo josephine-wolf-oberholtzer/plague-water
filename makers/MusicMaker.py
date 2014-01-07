5d8d80950307efd2da554945bf911bf05518a005
josiah-wolf-oberholtzer <josiah.oberholtzer@gmail.com>
2014-01-06 20:32:44 -0500

    Added fancier rhythm maker to music maker.

--- makers/MusicMaker.py
+++ makers/MusicMaker.py
@@ -21,14 +21,16 @@ class MusicMaker(abctools.AbjadObject):
         durations,
         context_hierarchy=None,
         context_name=None,
+        seed=None,
         ):
         assert len(durations)
         assert all(isinstance(x, Duration) for x in durations)
         assert isinstance(context_hierarchy,
             (datastructuretools.ContextHierarchy, type(None)))
         assert isinstance(context_name, (str, type(None)))
+        assert isinstance(seed, (int, type(None)))
         parameters = self._build_parameters(context_hierarchy, context_name)
-        music = self._build_music(durations, parameters)
+        music = self._build_music(durations, parameters, seed)
         return music
 
     def __eq__(self, expr):
@@ -39,14 +41,39 @@ class MusicMaker(abctools.AbjadObject):
 
     ### PRIVATE METHODS ###
 
-    def _build_music(self, durations, parameters):
-        note_maker = rhythmmakertools.NoteRhythmMaker()
-        music = note_maker(durations)
-        music = [Container(x) for x in music]
+    def _build_music(self, durations, parameters, seed):
+        durations = [x.pair for x in durations]
+        #rhythm_maker = rhythmmakertools.NoteRhythmMaker()
+        #rhythm_maker = rhythmmakertools.EqualDivisionRhythmMaker(
+        #    leaf_count=4,
+        #    beam_each_cell=False,
+        #    )
+        rhythm_maker = rhythmmakertools.OutputIncisedNoteRhythmMaker(
+            prefix_talea=[-2, -1, -3, -1, -2, -2],
+            prefix_lengths=[1, 1, 0, 1],
+            suffix_talea=[-1, -1, -2, -1, -1],
+            suffix_lengths=[1, 0, 1],
+            talea_denominator=32,
+            prolation_addenda=[0, 1, 1, 0, 2, 0],
+            secondary_divisions=[],
+            decrease_durations_monotonically=False,
+            tie_rests=False,
+            beam_each_cell=False,
+            beam_cells_together=False,
+            )
+        music = rhythm_maker(durations, seeds=seed)
+        for i, x in enumerate(music):
+            if isinstance(x, Tuplet) and x.is_trivial:
+                music[i] = Container()
+                music[i].extend(x)
+        music = [Container([x]) for x in music]
         music = Container(music)
-        beam = spannertools.DuratedComplexBeam(
-            durations=durations,
-            isolated_nib_direction=False,
+        beam = spannertools.GeneralizedBeam(
+            #durations=durations,
+            include_long_duration_notes=False,
+            include_long_duration_rests=False,
+            isolated_nib_direction=None,
+            use_stemlets=True,
             )
         attach(beam, music)
         return music
