743d57ebbc54a638e939df34554eb0213599a82d
josiah-wolf-oberholtzer <josiah.oberholtzer@gmail.com>
2014-01-12 16:44:54 -0500

    Added makers.DynamicsMaker.

---
+++ makers/DynamicsMaker.py
@@ -0,0 +1,25 @@
+# -*- encoding: utf-8 -*-
+from abjad import *
+from plague_water.makers.ContextAwareMaker import ContextAwareMaker
+
+
+class DynamicsMaker(ContextAwareMaker):
+
+    ### CLASS VARIABLES ###
+
+    __slots__ = (
+        )
+
+    ### SPECIAL METHODS ###
+
+    def __call__(
+        self,
+        music,
+        context_map=None,
+        context_name=None,
+        seed=None,
+        ):
+        assert isinstance(seed, (int, type(None)))
+        parameter_map = self._build_parameter_map(
+            context_map, context_name)
+        pass
