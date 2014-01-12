987cc82f233e0ae8a7f65fc9a70a414ffacd0801
josiah-wolf-oberholtzer <josiah.oberholtzer@gmail.com>
2014-01-12 16:45:09 -0500

    Added makers.PitchMaker.

---
+++ makers/PitchMaker.py
@@ -0,0 +1,34 @@
+# -*- encoding: utf-8 -*-
+from abjad import *
+from plague_water.makers.ContextAwareMaker import ContextAwareMaker
+
+
+class PitchMaker(ContextAwareMaker):
+
+    ### CLASS VARIABLES ###
+
+    __slots__ = (
+        )
+
+    ### SPECIAL METHODS ###
+
+    def __call__(
+        self,
+        music,
+        context_map=None,
+        context_name=None,
+        seed=None,
+        ):
+        assert isinstance(seed, (int, type(None)))
+        parameter_map = self._build_parameter_map(
+            context_map, context_name)
+        for logical_tie in iterate(music).by_logical_tie(pitched=True):
+            pitch_count = self.choose_pitch_count(parameter_map)
+            pitch_classes = self.choose_pitches(pitch_count, parameter_map)
+            for leaf in logical_tie:
+                if 1 < len(pitch_classes):
+                    chord = Chord(leaf)
+                    chord.written_pitches = pitch_classes
+                    mutate(leaf).replace(chord)
+                else:
+                    leaf.written_pitch = pitch_classes[0]
