7f77b259f9dc252d542c60a516c66b5001400a5d
josiah-wolf-oberholtzer <josiah.oberholtzer@gmail.com>
2014-01-13 13:49:27 -0500

    PayloadedTimespan.music_maker may now be None.

--- makers/PayloadedTimespan.py
+++ makers/PayloadedTimespan.py
@@ -19,7 +19,7 @@ class PayloadedTimespan(timespantools.Timespan):
         music_maker=None,
         ):
         from plague_water import makers
-        assert isinstance(music_maker, makers.MusicMaker)
+        assert isinstance(music_maker, (makers.MusicMaker, type(None)))
         timespantools.Timespan.__init__(
             self,
             start_offset=start_offset,
