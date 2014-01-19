cf88e2fddab53ec3580dea2df8a3ac8d4dc9e789
josiah-wolf-oberholtzer <josiah.oberholtzer@gmail.com>
2014-01-19 13:47:57 -0500

    Introduced shared palettes in segments/.

---
+++ segments/palette_a/definition.py
@@ -0,0 +1,30 @@
+# -*- encoding: utf-8 -*-
+from abjad import *
+from plague_water import makers
+from plague_water import materials
+from plague_water import score_templates
+
+### SEGMENT PARAMETERS ###
+
+permitted_time_signatures = materials.mixed_time_signatures
+segment_tempo = materials.tempo_inventory[0]
+
+### CONTEXT MAP ###
+
+score_template = score_templates.PlagueWaterScoreTemplate()
+context_map = datastructuretools.ContextMap(score_template)
+context_map['Plague Water Score']['playing_durations'] = [1, 1]
+context_map['Plague Water Score']['playing_groupings'] = [1, 1]
+context_map['Plague Water Score']['leading_rest_durations'] = [1, 1]
+context_map['Plague Water Score']['tailing_rest_durations'] = None
+
+### BRUSHES ###
+
+### SEGMENT DEFINITION ###
+
+segment_maker = makers.SegmentMaker(
+    context_map=context_map,
+    is_final_segment=False,
+    permitted_time_signatures=permitted_time_signatures,
+    segment_tempo=segment_tempo,
+    )
