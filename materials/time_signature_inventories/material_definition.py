acba4e4c8ef3397561151ef3ec3bbc7f5b01060c
josiah-wolf-oberholtzer <josiah.oberholtzer@gmail.com>
2014-01-21 18:00:40 -0500

    Updated time signature inventories.

--- materials/time_signature_inventories/material_definition.py
+++ materials/time_signature_inventories/material_definition.py
@@ -14,6 +14,29 @@ mixed_time_signatures = indicatortools.TimeSignatureInventory(
         ],
     )
 
+prime_time_signatures = indicatortools.TimeSignatureInventory(
+    [
+        (3, 16),
+        (5, 16),
+        (5, 8),
+        (7, 16),
+        (7, 8),
+        ],
+    )
+
+quick_time_signatures = indicatortools.TimeSignatureInventory(
+    [
+        (1, 16),
+        (1, 8),
+        (2, 8),
+        (1, 4),
+        (5, 16),
+        (2, 4),
+        (4, 8),
+        (5, 8),
+        ],
+    )
+
 round_time_signatures = indicatortools.TimeSignatureInventory(
     [
         (3, 4),
@@ -25,24 +48,17 @@ round_time_signatures = indicatortools.TimeSignatureInventory(
 
 square_time_signatures = indicatortools.TimeSignatureInventory(
     [
+        (2, 8),
         (2, 4),
         (4, 16),
         (4, 8),
         ],
     )
 
-prime_time_signatures = indicatortools.TimeSignatureInventory(
-    [
-        (5, 16),
-        (5, 8),
-        (7, 16),
-        (7, 8),
-        ],
-    )
-
 __all__ = (
     'mixed_time_signatures',
     'prime_time_signatures',
+    'quick_time_signatures',
     'round_time_signatures',
     'square_time_signatures',
     )
