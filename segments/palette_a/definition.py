8d4543d29a1e2f8f4817a630ebcc7df5b96abcf3
josiah-wolf-oberholtzer <josiah.oberholtzer@gmail.com>
2014-01-21 18:00:29 -0500

    Updated palettes.

--- segments/palette_a/definition.py
+++ segments/palette_a/definition.py
@@ -6,8 +6,9 @@ from plague_water import score_templates
 
 ### SEGMENT PARAMETERS ###
 
-permitted_time_signatures = materials.mixed_time_signatures
-segment_tempo = materials.tempo_inventory[0]
+measure_segmentation_talea = (1,)
+permitted_time_signatures = materials.square_time_signatures
+segment_tempo = indicatortools.Tempo(Duration(1, 4), 64)
 
 ### CONTEXT MAP ###
 
@@ -17,8 +18,6 @@ context_map = datastructuretools.ContextMap(score_template)
 short_duration_cursor = materials.short_duration_server()
 context_map[score]['playing_durations'] = materials.medium_duration_server()
 context_map[score]['playing_groupings'] = materials.short_grouping_server()
-#context_map[score]['leading_rest_durations'] = short_duration_cursor
-#context_map[score]['tailing_rest_durations'] = short_duration_cursor
 
 ### BRUSHES ###
 
@@ -31,7 +30,7 @@ piano_lh_brush = piano_rh_brush = makers.Brush([
 segment_maker = makers.SegmentMaker(
     context_map=context_map,
     is_final_segment=False,
-    measure_segmentation_talea=(2, 3),
+    measure_segmentation_talea=measure_segmentation_talea,
     permitted_time_signatures=permitted_time_signatures,
     piano_lh_brush=piano_lh_brush,
     piano_rh_brush=piano_rh_brush,
--- segments/palette_b/definition.py
+++ segments/palette_b/definition.py
@@ -6,25 +6,33 @@ from plague_water import score_templates
 
 ### SEGMENT PARAMETERS ###
 
-permitted_time_signatures = materials.mixed_time_signatures
-segment_tempo = materials.tempo_inventory[0]
+measure_segmentation_talea = (1,)
+permitted_time_signatures = materials.round_time_signatures
+segment_tempo = indicatortools.Tempo(Duration(1, 4), 72)
 
 ### CONTEXT MAP ###
 
 score_template = score_templates.PlagueWaterScoreTemplate()
+score = score_template()
 context_map = datastructuretools.ContextMap(score_template)
-context_map['Plague Water Score']['playing_durations'] = [1, 1]
-context_map['Plague Water Score']['playing_groupings'] = [1, 1]
-context_map['Plague Water Score']['leading_rest_durations'] = [1, 1]
-context_map['Plague Water Score']['tailing_rest_durations'] = None
+short_duration_cursor = materials.short_duration_server()
+context_map[score]['playing_durations'] = materials.medium_duration_server()
+context_map[score]['playing_groupings'] = materials.short_grouping_server()
 
 ### BRUSHES ###
+
+piano_lh_brush = piano_rh_brush = makers.Brush([
+    materials.staggering_music_maker,
+    ])
 
 ### SEGMENT DEFINITION ###
 
 segment_maker = makers.SegmentMaker(
     context_map=context_map,
     is_final_segment=False,
+    measure_segmentation_talea=measure_segmentation_talea,
     permitted_time_signatures=permitted_time_signatures,
+    piano_lh_brush=piano_lh_brush,
+    piano_rh_brush=piano_rh_brush,
     segment_tempo=segment_tempo,
     )
