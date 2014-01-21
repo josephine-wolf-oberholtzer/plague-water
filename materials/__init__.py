48a2ed3166b3f7267a08f3256944a39b01dda45d
josiah-wolf-oberholtzer <josiah.oberholtzer@gmail.com>
2014-01-21 13:25:27 -0500

    Renamed materials.brushes to materials.brush_components.

--- materials/__init__.py
+++ materials/__init__.py
@@ -1,11 +1,11 @@
 # -*- encoding: utf-8 -*-
 
-from brushes import *
+from brush_components import *
 from duration_servers import *
 from euler_numbers import *
-from registrations import *
 from pitch_classes import *
 from proportions import *
+from registrations import *
 from rhythm_makers import *
 from tempo_inventory import *
 from time_signature_inventories import *
