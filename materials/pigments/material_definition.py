b052f8550f54a1184102281e808056d1984cd299
josiah-wolf-oberholtzer <josiah.oberholtzer@gmail.com>
2014-01-21 16:56:56 -0500

    Updated pigments material.

--- materials/pigments/material_definition.py
+++ materials/pigments/material_definition.py
@@ -5,26 +5,38 @@ from plague_water import makers
 
 
 clanging_pigment = makers.Pigment(
-    playing_durations=None,
-    playing_groupings=None,
-    leading_rest_durations=None,
     music_maker=makers.MusicMaker(
-        minimum_timespan_duration=None,
+        leading_rest_durations=None,
+        minimum_timespan_duration=Duration(3, 16),
+        playing_durations=None,
+        playing_groupings=None,
         rhythm_maker=rhythmmakertools.TaleaRhythmMaker(
             talea=(4, 4, 4, 3, 4, 5, 4, 4, 3,),
             talea_denominator=16,
             ),
+        tailing_rest_durations=None,
         ),
-    tailing_rest_durations=None,
+    )
+
+
+droning_pigment = makers.Pigment(
+    music_maker=makers.MusicMaker(
+        leading_rest_durations=None,
+        minimum_timespan_duration=None,
+        playing_durations=None,
+        playing_groupings=None,
+        rhythm_maker=rhythmmakertools.NoteRhythmMaker(),
+        tailing_rest_durations=None,
+        ),
     )
 
 
 flowing_pigment = makers.Pigment(
-    playing_durations=None,
-    playing_groupings=None,
-    leading_rest_durations=None,
     music_maker=makers.MusicMaker(
-        minimum_timespan_duration=None,
+        leading_rest_durations=None,
+        minimum_timespan_duration=Duration(3, 16),
+        playing_durations=None,
+        playing_groupings=None,
         rhythm_maker=rhythmmakertools.RatioTaleaRhythmMaker(
             ratio_talea=(
                 (2, 1),
@@ -47,20 +59,24 @@ flowing_pigment = makers.Pigment(
                 ),
             tie_across_divisions=True,
             ),
+        tailing_rest_durations=None,
         ),
-    tailing_rest_durations=None,
     )
 
 
 staggering_pigment = makers.Pigment(
-    playing_durations=None,
-    playing_groupings=None,
-    leading_rest_durations=None,
     music_maker=makers.MusicMaker(
-        minimum_timespan_duration=None,
+        articulation_maker=makers.ArticulationMaker(
+            first_leaf=('>',),
+            inner_leaves=('.',),
+            ),
+        leading_rest_durations=None,
+        minimum_timespan_duration=Duration(3, 16),
+        playing_durations=None,
+        playing_groupings=None,
         rhythm_maker=rhythmmakertools.IncisedRhythmMaker(
             #body_ratio=(1, 1),
-            fill_with_notes=True,
+            fill_with_notes=False,
             incise_divisions=True,
             incise_specifier=rhythmmakertools.InciseSpecifier(
                 prefix_talea=(1, 1, 1, 1, 1, 1, 2, 1, 1),
@@ -70,17 +86,17 @@ staggering_pigment = makers.Pigment(
                 talea_denominator=32,
                 ),
             ),
+        tailing_rest_durations=None,
         ),
-    tailing_rest_durations=None,
     )
 
 
 stuttering_pigment = makers.Pigment(
-    playing_durations=None,
-    playing_groupings=None,
-    leading_rest_durations=None,
     music_maker=makers.MusicMaker(
+        leading_rest_durations=None,
         minimum_timespan_duration=None,
+        playing_durations=None,
+        playing_groupings=None,
         rhythm_maker=rhythmmakertools.RatioTaleaRhythmMaker(
             ratio_talea=(
                 (1, 1, -2),
@@ -96,17 +112,17 @@ stuttering_pigment = makers.Pigment(
                 ),
             tie_across_divisions=False,
             ),
+        tailing_rest_durations=None,
         ),
-    tailing_rest_durations=None,
     )
 
 
 winding_pigment = makers.Pigment(
-    playing_durations=None,
-    playing_groupings=None,
-    leading_rest_durations=None,
     music_maker=makers.MusicMaker(
-        minimum_timespan_duration=None,
+        leading_rest_durations=None,
+        minimum_timespan_duration=Duration(4, 16),
+        playing_durations=None,
+        playing_groupings=None,
         rhythm_maker=rhythmmakertools.TaleaRhythmMaker(
             burnish_output=True,
             burnish_specifier=rhythmmakertools.BurnishSpecifier(
@@ -120,13 +136,14 @@ winding_pigment = makers.Pigment(
             talea=(1, 2, 1, 1, 1, 1, 3, 2, 1, 2, 1, 3, 1, 1, 1, 2, 1,),
             talea_denominator=16,
             ),
+        tailing_rest_durations=None,
         ),
-    tailing_rest_durations=None,
     )
 
 
 __all__ = (
     'clanging_pigment',
+    'droning_pigment',
     'flowing_pigment',
     'staggering_pigment',
     'stuttering_pigment',
