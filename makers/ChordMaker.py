362d10f9fdbdd73d7e5edb788da6da2f394f8c70
josiah-wolf-oberholtzer <josiah.oberholtzer@gmail.com>
2014-01-23 14:03:07 -0500

    Integrated KeyClusterExpression and ChordExpression into ChordMaker.

--- makers/ChordMaker.py
+++ makers/ChordMaker.py
@@ -8,21 +8,17 @@ class ChordMaker(ContextAwareMaker):
     ### CLASS VARIABLES ###
 
     __slots__ = (
-        '_counts_talea',
-        '_intervals_talea',
+        '_talea',
         )
 
     ### INITIALIZER ###
 
     def __init__(
         self,
-        counts_talea=None,
-        intervals_talea=None,
+        talea=None,
         ):
-        counts_talea = self._coerce_cursor_argument(counts_talea)
-        intervals_talea = self._coerce_cursor_argument(intervals_talea)
-        self._counts_talea = counts_talea
-        self._intervals_talea = intervals_talea
+        talea = self._coerce_cursor_argument(talea)
+        self._talea = talea
 
     ### SPECIAL METHODS ###
 
@@ -32,30 +28,23 @@ class ChordMaker(ContextAwareMaker):
         seed=None,
         segment_actual_duration=None,
         ):
+        from plague_water import makers
+        expression_prototype = (
+            makers.ChordExpression,
+            makers.KeyClusterExpression,
+            type(None),
+            )
         iterator = iterate(music).by_logical_tie(pitched=True)
-        for i, logical_tie in enumerate(iterator):
-            count = self.counts_talea(1)[0]
-            if not count:
+        for logical_tie in enumerate(iterator):
+            expression = self.counts_talea()[0]
+            assert isinstance(expression, expression_prototype)
+            if expression is None:
                 continue
-            center_pitch = logical_tie[0].written_pitch
-            chord_pitches = set([center_pitch])
-            for interval_number in self.intervals_talea(count):
-                chord_pitch = center_pitch.transpose(interval_number)
-                chord_pitches.add(chord_pitch)
-            if 1 == len(chord_pitches):
-                continue
-            for leaf in logical_tie:
-                new_chord = Chord(leaf)
-                new_chord.written_pitches = tuple(chord_pitches)
-                mutate(leaf).replace(new_chord)
+            expression(logical_tie)
         assert inspect_(music).is_well_formed()
 
     ### PUBLIC PROPERTIES ###
 
     @property
-    def counts_talea(self):
-        return self._counts_talea
-
-    @property
-    def intervals_talea(self):
-        return self._intervals_talea
+    def talea(self):
+        return self._talea
