2cd3c30bc6421f5432bfd35bf30622075c04aac9
josiah-wolf-oberholtzer <josiah.oberholtzer@gmail.com>
2014-01-23 17:09:05 -0500

    Improved rhythm makers.

--- materials/music_makers/material_definition.py
+++ materials/music_makers/material_definition.py
@@ -10,10 +10,8 @@ clanging_music_maker = makers.MusicMaker(
     playing_durations=None,
     playing_groupings=None,
     rhythm_maker=rhythmmakertools.TaleaRhythmMaker(
-        beam_specifier=rhythmmakertools.BeamSpecifier(
-            beam_each_cell=False,
-            beam_cells_together=False,
-            ),
+        beam_each_cell=False,
+        beam_cells_together=False,
         talea=(4, 4, 4, 3, 4, 5, 4, 4, 3,),
         talea_denominator=16,
         ),
@@ -139,10 +137,8 @@ winding_music_maker = makers.MusicMaker(
     playing_durations=None,
     playing_groupings=None,
     rhythm_maker=rhythmmakertools.TaleaRhythmMaker(
-        beam_specifier=rhythmmakertools.BeamSpecifier(
-            beam_each_cell=False,
-            beam_cells_together=False,
-            ),
+        beam_each_cell=False,
+        beam_cells_together=False,
         burnish_output=True,
         burnish_specifier=rhythmmakertools.BurnishSpecifier(
             lefts=(-1,),
