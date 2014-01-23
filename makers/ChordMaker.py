2651c14cc2d2dbef02157e90dcde8e5191db16e1
josiah-wolf-oberholtzer <josiah.oberholtzer@gmail.com>
2014-01-23 13:16:53 -0500

    ChordMaker now uses cursors, rather than cyclic tuples for its talea.

--- makers/ChordMaker.py
+++ makers/ChordMaker.py
@@ -19,12 +19,8 @@ class ChordMaker(ContextAwareMaker):
         counts_talea=None,
         intervals_talea=None,
         ):
-        prototype = (tuple, type(None))
-        assert isinstance(counts_talea, prototype), counts_talea
-        assert isinstance(intervals_talea, prototype), intervals_talea
-        if counts_talea and intervals_talea:
-            assert sequencetools.all_are_nonnegative_integers(counts_talea) 
-            assert all(x != 0 for x in intervals_talea)
+        counts_talea = self._coerce_cursor_argument(counts_talea)
+        intervals_talea = self._coerce_cursor_argument(intervals_talea)
         self._counts_talea = counts_talea
         self._intervals_talea = intervals_talea
 
@@ -36,27 +32,14 @@ class ChordMaker(ContextAwareMaker):
         seed=None,
         segment_actual_duration=None,
         ):
-        if seed is None:
-            seed = 0
-        assert isinstance(seed, int)
-        counts_talea = self._expr_to_cyclic_tuple(
-            self.counts_talea,
-            seed,
-            )
-        intervals_talea = self._expr_to_cyclic_tuple(
-            self.intervals_talea,
-            seed,
-            )
-        intervals_cursor = datastructuretools.StatalServer(
-            intervals_talea)()
         iterator = iterate(music).by_logical_tie(pitched=True)
         for i, logical_tie in enumerate(iterator):
-            count = counts_talea[i]
+            count = self.counts_talea(1)[0]
             if not count:
                 continue
             center_pitch = logical_tie[0].written_pitch
             chord_pitches = set([center_pitch])
-            for interval_number in intervals_cursor(count):
+            for interval_number in self.intervals_talea(count):
                 chord_pitch = center_pitch.transpose(interval_number)
                 chord_pitches.add(chord_pitch)
             if 1 == len(chord_pitches):
@@ -68,7 +51,7 @@ class ChordMaker(ContextAwareMaker):
         assert inspect_(music).is_well_formed()
 
     ### PUBLIC PROPERTIES ###
-    
+
     @property
     def counts_talea(self):
         return self._counts_talea
