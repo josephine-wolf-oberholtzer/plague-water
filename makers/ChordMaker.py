7b7c91e8934dbdd1b4e3a2d53d1f27c7c007f7b7
josiah-wolf-oberholtzer <josiah.oberholtzer@gmail.com>
2014-01-24 12:05:22 -0500

    Revised ChordMaker and PitchClassMaker.

--- makers/ChordMaker.py
+++ makers/ChordMaker.py
@@ -26,13 +26,13 @@ class ChordMaker(Maker):
         talea = self._expr_to_cyclic_tuple(talea)
         self._ratio = ratio
         self._talea = talea
-        prototype = (
+        expression_prototype = (
             makers.ChordExpression,
             makers.KeyClusterExpression,
             type(None),
             )
         for sequence in talea:
-            assert all(isinstance(x, prototype) for x in sequence)
+            assert all(isinstance(x, expression_prototype) for x in sequence)
         self._talea_cursors = \
             self._ratio_and_talea_to_cursors(
                 ratio=ratio,
@@ -43,65 +43,26 @@ class ChordMaker(Maker):
 
     def __call__(
         self,
-        music,
-        seed=None,
+        logical_tie,
         segment_duration=None,
         ):
+        assert isinstance(logical_tie, selections.LogicalTie)
+        assert logical_tie
+        assert segment_duration
         offsets = self._duration_and_ratio_to_offsets(
             duration=segment_duration,
             ratio=self.ratio,
             )
-        iterator = iterate(music).by_logical_tie(pitched=True)
-        for logical_tie in enumerate(iterator):
-            start_offset = logical_tie.get_timespan().start_offset
-            index = self._offset_and_offsets_to_index(
-                offset=start_offset,
-                offsets=offsets,
-                )
-            cursor = self._talea_cursors[index]
-            expression = cursor()[0]
-            if expression is None:
-                continue
-            expression(logical_tie)
-        assert inspect_(music).is_well_formed()
-
-    ### PRIVATE METHODS ###
-
-    def _duration_and_ratio_to_offsets(
-        self,
-        duration=None,
-        ratio=None,
-        ):
-        ratio_sum = sum(self.ratio)
-        duration_parts = []
-        for ratio_part in self.ratio:
-            multiplier = Multiplier(ratio_part, ratio_sum)
-            duration_part = duration * multiplier
-            duration_parts.append(duration_part)
-        offsets = mathtools.cumulative_sums(duration_parts)
-        offsets = offsets[:-1]
-        return offsets
-
-    def _offset_and_offsets_to_index(
-        self,
-        offset=None,
-        offsets=None,
-        ):
-        if offset in offsets:
-            return offsets.index(offset)
-        return bisect.bisect(offsets, offset) - 1
-
-    def _ratio_and_talea_to_cursors(
-        self,
-        ratio=None,
-        talea=None,
-        ):
-        talea_cursors = []
-        for i in range(ratio):
-            subsequence = talea[i]
-            cursor = self._expr_to_statal_server_cursor(subsequence)
-            talea_cursors.append(cursor)
-        return tuple(talea_cursors)
+        start_offset = logical_tie.get_timespan().start_offset
+        index = self._offset_and_offsets_to_index(
+            offset=start_offset,
+            offsets=offsets,
+            )
+        cursor = self._talea_cursors[index]
+        expression = cursor()[0]
+        if expression is None:
+            continue
+        expression(logical_tie)
 
     ### PUBLIC PROPERTIES ###
 
--- makers/PitchClassMaker.py
+++ makers/PitchClassMaker.py
@@ -8,38 +8,119 @@ class PitchClassMaker(Maker):
     ### CLASS VARIABLES ###
 
     __slots__ = (
+        '_current_cursor',
+        '_last_pitch_class',
+        '_last_pitch_class_sequence_index',
+        '_last_transform_index',
+        '_pitch_class_ratio',
+        '_pitch_class_talea',
+        '_transform_ratio',
+        '_transform_talea',
         )
 
     ### INITIALIZER ###
 
     def __init__(
         self,
-        ratio=None,
-        talea=None,
+        pitch_class_ratio=None,
+        pitch_class_talea=None,
         transform_ratio=None,
         transform_talea=None,
         ):
-        pass
+        from plague_water import makers
+        pitch_class_ratio = mathtools.Ratio([abs(x) for x in ratio])
+        assert len(pitch_class_ratio)
+        if transform_ratio is not None:
+            transform_ratio = mathtools.Ratio([abs(x) for x in ratio])
+            assert len(transform_ratio)
+        self._pitch_class_ratio = pitch_class_ratio
+        self._transform_ratio = transform_ratio
+        pitch_class_prototype = (pitchtools.NumberedPitchClass,)
+        for sequence in pitch_class_talea:
+            assert all(isinstance(x, pitch_class_prototype)
+                for x in sequence)
+        self._pitch_class_talea = datastructuretools.CyclicTuple([
+            datastructuretools.CyclicTuple(sequence)
+            for sequence in pitch_class_talea
+            ])
+        if transform_talea is not None:
+            transform_prototype = (
+                makers.PitchClassTransformExpression,
+                type(None),
+                )
+            assert all(isinstance(x, transform_prototype)
+                for x in transform_talea)
+            self._transform_talea = self._expr_to_cyclic_tuple(transform_talea)
+        self._current_pitch_class_cursor = self._expr_to_statal_server_cursor(
+            self._pitch_class_talea[0])
+        self._last_pitch_class = None
+        self._last_pitch_class_sequence_index = None
+        self._last_transform_index = None
 
     ### SPECIAL METHODS ###
 
     def __call__(
         self,
-        music,
-        seed=None,
+        logical_tie,
         segment_duration=None,
         ):
-        assert isinstance(seed, (int, type(None)))
-        for logical_tie in iterate(music).by_logical_tie(pitched=True):
-            pitch_count = self.choose_pitch_count(parameter_map)
-            pitch_classes = self.choose_pitches(pitch_count, parameter_map)
-            for leaf in logical_tie:
-                if 1 < len(pitch_classes):
-                    chord = Chord(leaf)
-                    chord.written_pitches = pitch_classes
-                    mutate(leaf).replace(chord)
-                else:
-                    leaf.written_pitch = pitch_classes[0]
+        assert isinstance(logical_tie, selections.LogicalTie)
+        assert logical_tie
+        assert segment_duration
+        start_offset = logical_tie.get_timespan().start_offset
+        pitch_class_offsets = self._duration_and_ratio_to_offsets(
+            duration=segment_duration,
+            ratio=self.pitch_class_ratio,
+            )
+        pitch_class_sequence_index = self._offset_and_offsets_to_index(
+            offset=start_offset,
+            offsets=pitch_class_offsets,
+            )
+        if pitch_class_sequence_index != self._last_pitch_class_sequence_index:
+            self._last_pitch_class_sequence_index = pitch_class_sequence_index
+            pitch_class_sequence = \
+                self.pitch_class_talea[pitch_class_sequence_index]
+            self._current_cursor = self._expr_to_statal_server_cursor(
+                pitch_class_sequence)
+        if self.transform_talea is not None:
+            transform_offsets = self._duration_and_ratio_to_offsets(
+                duration=segment_duration,
+                ratio=self.transform_ratio,
+                )
+            transform_index = self._offset_offsets_to_index(
+                offset=start_offset,
+                offsets=transform_offsets,
+                )
+            if transform_index != self._last_transform_index:
+                self._last_transform_index = transform_index
+                transform = self.transform_talea[transform_index]
+                if transform is not None:
+                    pitch_class_sequence = self.pitch_class_talea[
+                        self._last_pitch_class_sequence_index]
+                    pitch_class_sequence = transform(pitch_class_sequence)
+                    self._current_cursor = self._expr_to_statal_server_cursor(
+                        pitch_class_sequence)
+        current_pitch_class = self._current_cursor(1)[0]
+        #while current_pitch_class == self._last_pitch_class:
+        #    current_pitch_class = self._current_cursor(1)[0]
+        self._last_pitch_class = current_pitch_class
+        for note in logical_tie:
+            note.written_pitch = current_pitch_class
 
     ### PUBLIC PROPERTIES ###
 
+    @property
+    def pitch_class_ratio(self):
+        return self._transform_talea
+
+    @property
+    def pitch_class_talea(self):
+        return self._transform_talea
+
+    @property
+    def transform_ratio(self):
+        return self._transform_talea
+
+    @property
+    def transform_talea(self):
+        return self._transform_talea
