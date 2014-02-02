56c36a13f64adc7e07108b5dae9a75c91b401aeb
josiah-wolf-oberholtzer <josiah.oberholtzer@gmail.com>
2014-02-02 17:18:16 -0500

    Generalizing Brush.

--- makers/Brush.py
+++ makers/Brush.py
@@ -12,20 +12,33 @@ class Brush(Maker):
     ### CLASS VARIABLES ###
 
     __slots__ = (
+        '_contexted_music_makers',
+        '_context_name',
+        '_context_dependencies',
         '_initial_music_maker',
         '_music_maker_indices',
         '_music_makers',
+        '_timespan_inventory',
         )
 
     ### INITIALIZER ###
 
     def __init__(
         self,
+        context_name=None,
+        context_dependencies=None,
         initial_music_maker=None,
         music_maker_indices=None,
         music_makers=None,
         ):
         from plague_water import makers
+        self._contexted_music_makers = set()
+        if context_name is not None:
+            context_name = str(context_name)
+        self._context_name = context_name
+        if context_dependencies is not None:
+            context_dependencies = tuple(str(x) for x in context_dependencies)
+        self._context_dependencies = context_dependencies
         assert isinstance(initial_music_maker, (makers.MusicMaker, type(None)))
         self._initial_music_maker = initial_music_maker
         if music_maker_indices is not None:
@@ -37,23 +50,27 @@ class Brush(Maker):
                 for x in music_makers)
             music_makers = tuple(music_makers)
         self._music_makers = music_makers
+        self._timespan_inventory = timespantools.TimespanInventory()
 
     ### SPECIAL METHODS ###
 
     def __call__(
         self,
         target_segment_duration,
-        segment_maker=None,  # TODO: decouple
         context_map=None,
         context_name=None,
+        dependencies=None,
         ):
+
+        self._contexted_music_makers = set()
+        self._timespan_inventory = timespantools.TimespanInventory()
+
         current_offset = durationtools.Offset(0)
         timespan_inventory = timespantools.TimespanInventory()
         message = '\t\t{}'.format(context_name)
         with systemtools.ProgressIndicator(message) as progress_indicator:
             if self.initial_music_maker is not None:
-                music_maker = segment_maker.get_cached_maker(
-                    self.initial_music_maker,
+                music_maker = self.initial_music_maker.from_context_map(
                     context_map=context_map,
                     context_name=context_name,
                     )
@@ -77,8 +94,7 @@ class Brush(Maker):
                 while current_offset < target_segment_duration:
                     music_maker_index = music_maker_indices[counter]
                     music_maker = music_makers[music_maker_index]
-                    music_maker = segment_maker.get_cached_maker(
-                        music_maker,
+                    music_maker = music_maker.from_context_map(
                         context_map=context_map,
                         context_name=context_name,
                         )
@@ -90,6 +106,7 @@ class Brush(Maker):
                     timespan_inventory.extend(music_maker_timespan_inventory)
                     counter += 1
                     progress_indicator.advance()
+
         return timespan_inventory
 
     ### PUBLIC METHODS ###
@@ -114,6 +131,14 @@ class Brush(Maker):
             )
 
     ### PUBLIC PROPERTIES ###
+
+    @property
+    def context_name(self):
+        return self._context_name
+
+    @property
+    def context_dependencies(self):
+        return self._context_dependencies
 
     @property
     def initial_music_maker(self):
