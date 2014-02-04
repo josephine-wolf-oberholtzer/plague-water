510814dcad60182f4e57cc7cff41c16549776bd3
josiah-wolf-oberholtzer <josiah.oberholtzer@gmail.com>
2014-02-04 18:45:23 -0500

    Removed unused LifelineStrategy class.

--- makers/LifelineStrategy.py
+++
@@ -1,9 +0,0 @@
-# -*- encoding: utf-8 -*-
-from abjad import *
-from plague_water.makers.Maker import Maker
-
-
-class LifelineStrategy(Maker):
-
-    def __call__(self, timespan_inventory):
-        return None
