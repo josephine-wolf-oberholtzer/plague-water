d9cfb00ef05ef2f1de63e900b9a7d2f680f2c5e7
josiah-wolf-oberholtzer <josiah.oberholtzer@gmail.com>
2014-02-04 18:37:08 -0500

    Implemented cyclical logical tie spanner application in SpannerMaker.

--- makers/SpannerMaker.py
+++ makers/SpannerMaker.py
@@ -2,7 +2,9 @@
 import copy
 from abjad import attach
 from abjad import iterate
+from abjad.tools import datastructuretools
 from abjad.tools import durationtools
+from abjad.tools import sequencetools
 from abjad.tools import spannertools
 from plague_water.makers.Maker import Maker
 
@@ -12,6 +14,7 @@ class SpannerMaker(Maker):
     ### CLASS VARIABLES ###
 
     __slots__ = (
+        '_cyclical_logical_tie_spanners',
         '_division_spanners',
         '_output_spanners',
         '_logical_tie_spanners',
@@ -22,11 +25,14 @@ class SpannerMaker(Maker):
 
     def __init__(
         self,
+        cyclical_logical_tie_spanners=None,
         division_spanners=None,
         logical_tie_spanners=None,
         minimum_logical_tie_duration=None,
         output_spanners=None,
         ):
+        self._cyclical_logical_tie_spanners = self._none_to_tuple(
+            cyclical_logical_tie_spanners)
         self._division_spanners = self._none_to_tuple(division_spanners)
         self._logical_tie_spanners = self._none_to_tuple(logical_tie_spanners)
         self._output_spanners = self._none_to_tuple(output_spanners)
@@ -46,12 +52,18 @@ class SpannerMaker(Maker):
         if seed is None:
             seed = 0
         assert isinstance(seed, int)
-        output_spanners = self._prepare_spanners(
-            self.output_spanners)
+        cyclical_logical_tie_spanners = self._prepare_spanners(
+            self.cyclical_logical_tie_spanners)
+        cyclical_logical_tie_spanners = sequencetools.rotate_sequence(
+            cyclical_logical_tie_spanners)
+        cyclical_logical_tie_spanners = datastructuretools.CyclicTuple(
+            cyclical_logical_tie_spanners)
         division_spanners = self._prepare_spanners(
             self.division_spanners)
         logical_tie_spanners = self._prepare_spanners(
             self.logical_tie_spanners)
+        output_spanners = self._prepare_spanners(
+            self.output_spanners)
         if output_spanners:
             for spanner in output_spanners:
                 spanner = copy.copy(spanner)
@@ -61,16 +73,20 @@ class SpannerMaker(Maker):
                 for spanner in division_spanners:
                     spanner = copy.copy(spanner)
                     attach(spanner, music)
-        if logical_tie_spanners:
+        if logical_tie_spanners or cyclical_logical_tie_spanners:
+            count = 0
             for logical_tie in iterate(music).by_logical_tie(pitched=True):
                 if self.minimum_logical_tie_duration is not None:
                     tie_duration = logical_tie.get_duration()
                     if tie_duration < self.minimum_tie_duration:
                         continue
-                spanner = copy.copy(spanner)
                 for spanner in logical_tie_spanners:
                     spanner = copy.copy(spanner)
                     attach(spanner, logical_tie)
+                if cyclical_logical_tie_spanners:
+                    spanner = copy.copy(cyclical_logical_tie_spanners[count])
+                    attach(spanner, logical_tie)
+                count += 1
 
     ### PRIVATE METHODS ###
 
@@ -100,6 +116,10 @@ class SpannerMaker(Maker):
         return expr
 
     ### PUBLIC PROPERTIES ###
+
+    @property
+    def cyclical_logical_tie_spanners(self):
+        return self._cyclical_logical_tie_spanners
 
     @property
     def division_spanners(self):
