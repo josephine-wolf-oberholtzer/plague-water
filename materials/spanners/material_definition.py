9d312dd03ca18f9bf3925cbd8e8222419711c8ac
josiah-wolf-oberholtzer <josiah.oberholtzer@gmail.com>
2014-03-09 18:54:20 -0400

    Improved guero glissandi.

--- materials/spanners/material_definition.py
+++ materials/spanners/material_definition.py
@@ -5,9 +5,11 @@ from abjad.tools import spannertools
 
 key_glissando_spanner = spannertools.Glissando()
 override(key_glissando_spanner).accidental.stencil = False
+override(key_glissando_spanner).accidental_cautionary.stencil = False
 override(key_glissando_spanner).glissando.bound_details__left__padding = 1.5
 override(key_glissando_spanner).glissando.bound_details__right__end_on_accidental = False
 override(key_glissando_spanner).glissando.bound_details__right__padding = 1.5
+override(key_glissando_spanner).note_head.no_ledgers = True
 override(key_glissando_spanner).note_head.style = 'cross'
 override(key_glissando_spanner).tie.stencil = False
 
